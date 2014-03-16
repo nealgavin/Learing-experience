4d7d81465673d9b8935f3cea86de80151315f26c
nealgavin <nealgavin@126.com>
2014-03-16 20:58:48 +0800

    python

---
+++ Python/2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+for x in ['aa',234,3.44,'apple']:
+	print x,"hh"
+x=int(raw_input("intput:"))
+def fun():
+	return 0
+if 3<4:
+	print "sdf"
+	print "fuck you "
+else :print " fuck"
+if fun():
+	print "haha I love you !"
+
+
+else: 
+	print "sdfghjhgfd"
+if x > 8:
+	print "big 8"
+elif x<8:
+	print "<8"
